be34e8d5b52c535e0a16c0d8d1bba3825762fcb2
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-02 17:44:58 +0500

    created equipment class

---
+++ FilmStudio/FilmStudio/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStudio
+{
+    class Equipment
+    {
+        private int Id, QtyAvailable, QtyBooked;
+        private String Description, Make, Mode, Remarks;
+
+        public Equipment()
+        {
+            Id = 0;
+        }
+    }
+}
