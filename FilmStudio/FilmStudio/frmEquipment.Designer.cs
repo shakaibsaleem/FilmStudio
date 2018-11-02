d11a0fa072efb07cd4f19d0240b0df3fd8143b83
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-02 19:32:17 +0500

    frmEquipment layout updated

--- FilmStudio/FilmStudio/frmEquipment.Designer.cs
+++ FilmStudio/FilmStudio/frmEquipment.Designer.cs
@@ -42,12 +42,21 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBoxItem = new System.Windows.Forms.GroupBox();
+            this.groupBoxItem.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblItemId
             // 
             this.lblItemId.AutoSize = true;
-            this.lblItemId.Location = new System.Drawing.Point(12, 15);
+            this.lblItemId.Location = new System.Drawing.Point(6, 22);
             this.lblItemId.Name = "lblItemId";
             this.lblItemId.Size = new System.Drawing.Size(42, 13);
             this.lblItemId.TabIndex = 0;
@@ -55,22 +64,22 @@
             // 
             // txtItemId
             // 
-            this.txtItemId.Location = new System.Drawing.Point(113, 12);
+            this.txtItemId.Location = new System.Drawing.Point(107, 19);
             this.txtItemId.Name = "txtItemId";
-            this.txtItemId.Size = new System.Drawing.Size(167, 20);
+            this.txtItemId.Size = new System.Drawing.Size(448, 20);
             this.txtItemId.TabIndex = 1;
             // 
             // txtDescription
             // 
-            this.txtDescription.Location = new System.Drawing.Point(113, 39);
+            this.txtDescription.Location = new System.Drawing.Point(107, 45);
             this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(167, 20);
+            this.txtDescription.Size = new System.Drawing.Size(448, 20);
             this.txtDescription.TabIndex = 2;
             // 
             // lblDescription
             // 
             this.lblDescription.AutoSize = true;
-            this.lblDescription.Location = new System.Drawing.Point(12, 42);
+            this.lblDescription.Location = new System.Drawing.Point(6, 48);
             this.lblDescription.Name = "lblDescription";
             this.lblDescription.Size = new System.Drawing.Size(63, 13);
             this.lblDescription.TabIndex = 3;
@@ -78,43 +87,43 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 66);
+            this.textBox1.Location = new System.Drawing.Point(107, 71);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.Size = new System.Drawing.Size(448, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(113, 93);
+            this.textBox2.Location = new System.Drawing.Point(107, 97);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(167, 20);
+            this.textBox2.Size = new System.Drawing.Size(448, 20);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(113, 120);
+            this.textBox3.Location = new System.Drawing.Point(107, 123);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(167, 20);
+            this.textBox3.Size = new System.Drawing.Size(448, 20);
             this.textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(113, 147);
+            this.textBox4.Location = new System.Drawing.Point(107, 149);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(167, 20);
+            this.textBox4.Size = new System.Drawing.Size(448, 20);
             this.textBox4.TabIndex = 7;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(113, 174);
+            this.textBox5.Location = new System.Drawing.Point(107, 175);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(167, 20);
+            this.textBox5.Size = new System.Drawing.Size(448, 20);
             this.textBox5.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Location = new System.Drawing.Point(6, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 9;
@@ -123,7 +132,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Location = new System.Drawing.Point(6, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 10;
@@ -132,7 +141,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Location = new System.Drawing.Point(6, 126);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 13);
             this.label3.TabIndex = 11;
@@ -141,7 +150,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Location = new System.Drawing.Point(6, 152);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 13);
             this.label4.TabIndex = 12;
@@ -150,36 +159,118 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 177);
+            this.label5.Location = new System.Drawing.Point(6, 178);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(52, 13);
             this.label5.TabIndex = 13;
             this.label5.Text = "Remarks:";
             // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(12, 219);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(93, 219);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 15;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(174, 219);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 16;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(255, 219);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(336, 219);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(417, 219);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(498, 219);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxItem
+            // 
+            this.groupBoxItem.Controls.Add(this.lblItemId);
+            this.groupBoxItem.Controls.Add(this.txtItemId);
+            this.groupBoxItem.Controls.Add(this.txtDescription);
+            this.groupBoxItem.Controls.Add(this.lblDescription);
+            this.groupBoxItem.Controls.Add(this.textBox1);
+            this.groupBoxItem.Controls.Add(this.textBox2);
+            this.groupBoxItem.Controls.Add(this.textBox3);
+            this.groupBoxItem.Controls.Add(this.textBox4);
+            this.groupBoxItem.Controls.Add(this.label5);
+            this.groupBoxItem.Controls.Add(this.textBox5);
+            this.groupBoxItem.Controls.Add(this.label4);
+            this.groupBoxItem.Controls.Add(this.label1);
+            this.groupBoxItem.Controls.Add(this.label3);
+            this.groupBoxItem.Controls.Add(this.label2);
+            this.groupBoxItem.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxItem.Name = "groupBoxItem";
+            this.groupBoxItem.Size = new System.Drawing.Size(561, 201);
+            this.groupBoxItem.TabIndex = 21;
+            this.groupBoxItem.TabStop = false;
+            this.groupBoxItem.Text = "Item Details";
+            // 
             // frmEquipment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 461);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.lblDescription);
-            this.Controls.Add(this.txtDescription);
-            this.Controls.Add(this.txtItemId);
-            this.Controls.Add(this.lblItemId);
+            this.ClientSize = new System.Drawing.Size(585, 254);
+            this.Controls.Add(this.groupBoxItem);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnClose);
             this.Name = "frmEquipment";
             this.Text = "Equipment Form";
             this.Load += new System.EventHandler(this.frmEquipment_Load);
+            this.groupBoxItem.ResumeLayout(false);
+            this.groupBoxItem.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -199,6 +290,14 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox groupBoxItem;
     }
 }
 
--- FilmStudio/FilmStudio/frmEquipment.cs
+++ FilmStudio/FilmStudio/frmEquipment.cs
@@ -21,5 +21,10 @@ namespace FilmStudio
         {
 
         }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
