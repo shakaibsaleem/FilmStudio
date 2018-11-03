10b8c41c320e7abbcdadac4b62008af62423f77d
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-03 15:28:38 +0500

    Created connection class

---
+++ FilmStudio/FilmStudio/mySQLcon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace FilmStudio
+{
+    public class mySQLcon
+    {
+        public SqlConnection con;
+
+        public mySQLcon()
+        {
+            con = new SqlConnection();
+            con.ConnectionString = "Data Source = SHAKAIBLAPTOP\SQLEXPRESS;Initial Catalog = FilmStudio; Persist Security Info=True;User ID = sa; Password=uiop7890;";
+            con.Open();
+        }
+    }
+}
