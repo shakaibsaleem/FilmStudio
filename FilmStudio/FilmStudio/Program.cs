af689e19498dd31f1934e2942f7f424a26928744
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-05 12:57:17 +0500

    insig change to progrom.cs

--- FilmStudio/FilmStudio/Program.cs
+++ FilmStudio/FilmStudio/Program.cs
@@ -16,8 +16,8 @@ namespace FilmStudio
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new frmEquipment());
             Application.Run(new frmLogin());
+            //Application.Run(new frmEquipment());
         }
     }
 }
