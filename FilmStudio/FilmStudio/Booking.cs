a7f3eb8d9291b0b6c3b56b881e4f1140453ea436
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-07 09:48:14 +0500

    Update FilmStudio/FilmStudio/Booking.cs

--- FilmStudio/FilmStudio/Booking.cs
+++ FilmStudio/FilmStudio/Booking.cs
@@ -8,7 +8,32 @@ namespace FilmStudio
 {
     public class Booking
     {
-        public int Id;
-        //date time ???
+        //Student student;
+        //User user;
+        public int ID;
+        public DateTime IssuedOn, DueOn, ReturnedOn, BookedOn;
+        public string Notes, Project;
+
+        public Booking()
+        {
+            ID = 0;
+            IssuedOn = new DateTime(2018,11,8,9,0,0,0);
+            DueOn = new DateTime(2018, 11, 11, 9, 0, 0, 0);
+            ReturnedOn = new DateTime(2018, 11, 10, 10, 0, 0, 0);
+            BookedOn = new DateTime(2018, 11, 7, 9, 41, 15, 2);
+            Notes = "";
+            Project = "";
+        }
+
+        public Booking(int iD, DateTime issuedOn, DateTime dueOn, DateTime returnedOn, DateTime bookedOn, string notes, string project)
+        {
+            ID = iD;
+            IssuedOn = issuedOn;
+            DueOn = dueOn;
+            ReturnedOn = returnedOn;
+            BookedOn = bookedOn;
+            Notes = notes ?? throw new ArgumentNullException(nameof(notes));
+            Project = project ?? throw new ArgumentNullException(nameof(project));
+        }
     }
 }
