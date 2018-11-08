26f8896a4670bf763c630c1927840ca0926e7e8e
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-08 14:46:05 +0500

    Added Instructor class

---
+++ FilmStudio/FilmStudio/Instructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStudio
+{
+    public class Instructor
+    {
+        public int InstructorID;
+        public string InstructorName;
+
+        public Instructor()
+        {
+            InstructorID = 0;
+            InstructorName = "NewInstructor";
+        }
+
+        public Instructor(int instructorID, string instructorName)
+        {
+            InstructorID = instructorID;
+            InstructorName = instructorName ?? throw new ArgumentNullException(nameof(instructorName));
+        }
+    }
+}
