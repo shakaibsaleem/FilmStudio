1ee3ebad6927f14c9bd02680353d18111e8ecda3
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-08 14:45:54 +0500

    Added Course class

---
+++ FilmStudio/FilmStudio/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStudio
+{
+    public class Course
+    {
+        public int CourseID;
+        public string CourseName, CourseCode;
+
+        public Course()
+        {
+            CourseID = 0;
+            CourseName = "NewCourse";
+            CourseCode = "CODE101";
+        }
+
+        public Course(int courseID, string courseName, string courseCode)
+        {
+            CourseID = courseID;
+            CourseName = courseName ?? throw new ArgumentNullException(nameof(courseName));
+            CourseCode = courseCode ?? throw new ArgumentNullException(nameof(courseCode));
+        }
+    }
+}
