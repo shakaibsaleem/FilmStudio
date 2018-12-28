e961372acd4ce3eb275c24e57f8a4fe1da150834
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-28 21:13:36 +0500

    Update User.cs
    UserID changed from int to string to ease exchange between textbox and query

--- FilmStudio/User.cs
+++ FilmStudio/User.cs
@@ -8,22 +8,21 @@ namespace FilmStudio
 {
     public class User
     {
-        public int UserID;
-        public string Name, Username, Passkey;
+        public string UserID, Name, Username, Passkey;
         public bool IsAdmin;
 
         public User()
         {
-            UserID = 9211;
+            UserID = "1";
             Name = "Shakaib Saleem";
             Username = "shakaib";
-            Passkey = "passkey";
-            IsAdmin = false;
+            Passkey = "admin";
+            IsAdmin = true;
         }
 
-        public User(int userID, string name, string username, string passkey, bool isAdmin)
+        public User(string userID, string name, string username, string passkey, bool isAdmin)
         {
-            UserID = userID;
+            UserID = userID ?? throw new ArgumentNullException(nameof(userID));
             Name = name ?? throw new ArgumentNullException(nameof(name));
             Username = username ?? throw new ArgumentNullException(nameof(username));
             Passkey = passkey ?? throw new ArgumentNullException(nameof(passkey));
