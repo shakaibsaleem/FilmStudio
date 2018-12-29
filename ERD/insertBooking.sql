e187b29bc6e63020bd44e2dd34d7a0e10b4231bd
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-29 12:16:39 +0500

    Update insertBooking.sql

--- ERD/insertBooking.sql
+++ ERD/insertBooking.sql
@@ -1,11 +1,14 @@
-
+		
 insert into Bookings (UserID,Notes,BookedBy,BookingDate,BookingTime,IssueDate,IssueTime,DueDate,DueTime,ReturnDate,ReturnTime)
-values (1, 'note','Student','2018-12-29','11:1:2','2018-12-29','12:00:00','2018-12-30','09:00:00','2018-12-30','08:00:00')
+values (1, 'note','Instructor','2018-12-30','11:1:2','2018-12-29','12:00:00','2018-12-30','09:00:00','2018-12-30','08:00:00')
 
 select * from Bookings
+
+select top 1 BookingID from Bookings order by BookingID desc
 
 select * from Bookings where BookingDate = '2018-12-29'
 
 select BookingTime from Bookings where BookingTime = '11:1:2'
 
-delete from Bookings where BookingDate = '2018-12-29'
+delete from Bookings where BookingDate = '2018-12-30'
+
