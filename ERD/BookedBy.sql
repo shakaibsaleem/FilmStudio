32dfb05db7d628d1077039fb9104944b473cf475
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-29 14:36:04 +0500

    Update BookedBy.sql

--- ERD/BookedBy.sql
+++ ERD/BookedBy.sql
@@ -6,6 +6,11 @@ values ('Aaron Mulvany','aaron.mulvany@ahss.habib.edu.pk','03210321321')
 select * from Instructors
 
 insert into BookingsByInstructors (InstructorID,BookingID)
-values (1,2)
+values (1,11)
+
+delete from BookingsByInstructors where BookingID = 11
 
 select * from BookingsByInstructors
+
+select * from Bookings
+
