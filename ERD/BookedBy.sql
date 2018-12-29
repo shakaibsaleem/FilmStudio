66ebbc919e6a77b9e68d108d97f7761e119e6bc5
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-29 12:16:34 +0500

    Create BookedBy.sql

---
+++ ERD/BookedBy.sql
@@ -0,0 +1,11 @@
+select * from BookingsByInstructors where BookingID = 2
+
+insert into Instructors (Name, Email,Contact)
+values ('Aaron Mulvany','aaron.mulvany@ahss.habib.edu.pk','03210321321')
+
+select * from Instructors
+
+insert into BookingsByInstructors (InstructorID,BookingID)
+values (1,2)
+
+select * from BookingsByInstructors
