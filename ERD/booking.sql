b45d385f1af6cb8aa9fd0ed41bdc8a5fb80ae21c
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-30 13:25:00 +0500

    files merged incorporating updates

--- ERD/booking.sql
+++ ERD/booking.sql
@@ -1,4 +1,20 @@
-		
+
+select * from BookingsByInstructors where BookingID = 2
+
+insert into Instructors (Name, Email,Contact)
+values ('Aaron Mulvany','aaron.mulvany@ahss.habib.edu.pk','03210321321')
+
+select * from Instructors
+
+insert into BookingsByInstructors (InstructorID,BookingID)
+values (1,11)
+
+delete from BookingsByInstructors where BookingID = 11
+
+select * from BookingsByInstructors
+
+select * from Bookings
+
 insert into Bookings (UserID,Notes,BookedBy,BookingDate,BookingTime,IssueDate,IssueTime,DueDate,DueTime,ReturnDate,ReturnTime)
 values (1, 'note','Instructor','2018-12-30','11:1:2','2018-12-29','12:00:00','2018-12-30','09:00:00','2018-12-30','08:00:00')
 
@@ -11,4 +27,3 @@ select * from Bookings where BookingDate = '2018-12-29'
 select BookingTime from Bookings where BookingTime = '11:1:2'
 
 delete from Bookings where BookingDate = '2018-12-30'
-
