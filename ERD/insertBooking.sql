1a81f73655b6d75bfbbdc9553c9d751932a9a758
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-30 13:22:15 +0500

    Revert "files merged"
    This reverts commit d5880042c9569c1362723be4e0e50323fb0124d7.

--- ERD/insertBooking.sql
+++ ERD/insertBooking.sql
@@ -12,15 +12,3 @@ select BookingTime from Bookings where BookingTime = '11:1:2'
 
 delete from Bookings where BookingDate = '2018-12-30'
 
-select * from BookingsByInstructors where BookingID = 2
-
-insert into Instructors (Name, Email,Contact)
-values ('Aaron Mulvany','aaron.mulvany@ahss.habib.edu.pk','03210321321')
-
-select * from Instructors
-
-insert into BookingsByInstructors (InstructorID,BookingID)
-values (1,2)
-
-select * from BookingsByInstructors
-
