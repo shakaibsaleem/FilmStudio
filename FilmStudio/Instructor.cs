64b44cbfac2b124097009f8c5daee63ea0eea2b2
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-12-31 17:34:05 +0500

    Update Instructor.cs
    changes made to reflect changes to erd

--- FilmStudio/Instructor.cs
+++ FilmStudio/Instructor.cs
@@ -8,19 +8,21 @@ namespace FilmStudio
 {
     public class Instructor
     {
-        public string InstructorID, Name, Email, Contact;
+        public string ID, HabibID, Name, Email, Contact;
 
         public Instructor()
         {
-            InstructorID = "1";
-            Name = "NewInstructor";
-            Email = "unknown";
-            Contact = "NIL";
+            ID = "1";
+            HabibID = "aaron.mulvany";
+            Name = "Aaron Mulvany";
+            Email = "aaron.mulvany@ahss.habib.edu.pk";
+            Contact = "+923210123456";
         }
 
-        public Instructor(string instructorID, string name, string email, string contact)
+        public Instructor(string iD, string habibID, string name, string email, string contact)
         {
-            InstructorID = instructorID ?? throw new ArgumentNullException(nameof(instructorID));
+            ID = iD ?? throw new ArgumentNullException(nameof(iD));
+            HabibID = habibID ?? throw new ArgumentNullException(nameof(habibID));
             Name = name ?? throw new ArgumentNullException(nameof(name));
             Email = email ?? throw new ArgumentNullException(nameof(email));
             Contact = contact ?? throw new ArgumentNullException(nameof(contact));
