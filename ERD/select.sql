a2e619eafb24c1c4778022183fb5e31b00c54900
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-06 01:48:44 +0500

    Create select.sql

---
+++ ERD/select.sql
@@ -0,0 +1,13 @@
+
+select * from BookedItems
+select * from Bookings
+select * from BookingsByInstructors
+select * from BookingsByStaff
+select * from BookingsByStudents
+select * from Courses
+select * from Enrolments
+select * from Equipments
+select * from Instructors
+select * from Staff
+select * from Students
+select * from Users
