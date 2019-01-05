5c57a8cfa4604b7a23b22370d846411c2c46aef9
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-06 04:20:22 +0500

    Created frmUser
    Form created, fields updating, not connected to backend yet

---
+++ FilmStudio/frmUser.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace FilmStudio
+{
+    partial class frmUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxAdmin = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(81, 38);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 4;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(81, 64);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 5;
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // checkBoxAdmin
+            // 
+            this.checkBoxAdmin.AutoSize = true;
+            this.checkBoxAdmin.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxAdmin.Location = new System.Drawing.Point(12, 90);
+            this.checkBoxAdmin.Name = "checkBoxAdmin";
+            this.checkBoxAdmin.Size = new System.Drawing.Size(83, 17);
+            this.checkBoxAdmin.TabIndex = 7;
+            this.checkBoxAdmin.Text = "Admin rights";
+            this.checkBoxAdmin.UseVisualStyleBackColor = true;
+            this.checkBoxAdmin.CheckedChanged += new System.EventHandler(this.checkBoxAdmin_CheckedChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(188, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(188, 43);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(188, 72);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(188, 102);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 11;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(188, 132);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 12;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(188, 161);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 13;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(188, 190);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 228);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.checkBoxAdmin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "frmUser";
+            this.Text = "User Details Form";
+            this.Load += new System.EventHandler(this.frmUser_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxAdmin;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ FilmStudio/frmUser.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FilmStudio
+{
+    public partial class frmUser : Form
+    {
+        public User myUser;
+        mySQLcon myCon;
+        SqlConnection con;
+        string state;
+
+        public frmUser()
+        {
+            InitializeComponent();
+            myCon = new mySQLcon();
+            con = myCon.con;
+            myUser = new User();
+            state = "Empty";
+        }
+
+        public frmUser(string id)
+        {
+            InitializeComponent();
+            myCon = new mySQLcon();
+            con = myCon.con;
+            myUser = new User();
+            LoadRecord(id);
+            state = "View";
+        }
+
+        private void frmUser_Load(object sender, EventArgs e)
+        {
+            UpdateFields(state);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            state = "Incomplete";
+            UpdateFields(state);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            state = "View";
+            UpdateFields(state);
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            state = "Incomplete";
+            UpdateFields(state);
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            myUser.Name = txtName.Text;
+            CheckFilled();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            myUser.Username = txtUsername.Text;
+            CheckFilled();
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        {
+            myUser.Passkey = txtPass.Text;
+            CheckFilled();
+        }
+
+        private void checkBoxAdmin_CheckedChanged(object sender, EventArgs e)
+        {
+            myUser.IsAdmin = checkBoxAdmin.Checked;
+        }
+
+        public void LoadRecord(string id)
+        {
+            SqlDataReader rd;
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            string str1 = "";
+            try
+            {
+                cmd.CommandText = "select Name,Username,Passkey,isAdmin from Users where UserID = " + id;
+                rd = cmd.ExecuteReader();
+                if (rd.Read())
+                {
+                    myUser.ID = id;
+                    myUser.Name = rd[0].ToString();
+                    myUser.Username = rd[1].ToString();
+                    myUser.Passkey = rd[2].ToString();
+                    str1 = rd[3].ToString();
+                }
+                rd.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in LoadRecord()");
+                return;
+            }
+            myUser.IsAdmin = str1 == "1";
+
+            txtName.Text = myUser.Name;
+            txtPass.Text = myUser.Passkey;
+            //txtPass.PasswordChar = '*';
+            txtUsername.Text = myUser.Username;
+            checkBoxAdmin.Checked = myUser.IsAdmin;
+        }
+
+        public void UpdateFields(string s)
+        {
+            if (s == "View")
+            {
+                btnAdd.Enabled = false;
+                btnClose.Enabled = true;
+                btnPrevious.Enabled = true;
+                btnNext.Enabled = true;
+                btnEdit.Enabled = true;
+                btnSave.Enabled = false;
+                btnDelete.Enabled = true;
+
+                txtName.Enabled = true;
+                txtPass.Enabled = true;
+                txtUsername.Enabled = true;
+                checkBoxAdmin.Enabled = true;
+
+                txtName.ReadOnly = true;
+                txtPass.ReadOnly = true;
+                txtUsername.ReadOnly = true;
+                txtPass.PasswordChar = '*';
+
+                checkBoxAdmin.AutoCheck = false;
+            }
+            else if (s == "Empty")
+            {
+                btnAdd.Enabled = true;
+                btnClose.Enabled = true;
+                btnDelete.Enabled = false;
+                btnEdit.Enabled = false;
+                btnNext.Enabled = false;
+                btnPrevious.Enabled = false;
+                btnSave.Enabled = false;
+
+                txtName.Enabled = false;
+                txtPass.Enabled = false;
+                txtUsername.Enabled = false;
+                checkBoxAdmin.Enabled = false;
+            }
+            else if (s == "Filled")
+            {
+                btnSave.Enabled = true;
+                checkBoxAdmin.AutoCheck = true;
+            }
+            else if (s == "Incomplete")
+            {
+                btnAdd.Enabled = false;
+                btnClose.Enabled = true;
+                btnEdit.Enabled = false;
+                btnNext.Enabled = true;
+                btnPrevious.Enabled = true;
+                btnSave.Enabled = false;
+                btnDelete.Enabled = true;
+
+                txtName.Enabled = true;
+                txtPass.Enabled = true;
+                txtUsername.Enabled = true;
+                checkBoxAdmin.Enabled = true;
+
+                txtName.ReadOnly = false;
+                txtPass.ReadOnly = false;
+                txtUsername.ReadOnly = false;
+                //txtPass.PasswordChar = '\0';
+
+                checkBoxAdmin.AutoCheck = true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid argument: " + s, "Error in UpdateFields()");
+            }
+        }
+
+        public void CheckFilled()
+        {
+            if (txtName.Text.Length==0)
+            {
+                state = "Incomplete";
+            }
+            else if (txtPass.Text.Length == 0)
+            {
+                state = "Incomplete";
+            }
+            else if (txtUsername.Text.Length == 0)
+            {
+                state = "Incomplete";
+            }
+            else
+            {
+                state = "Filled";
+            }
+            UpdateFields(state);
+        }
+    }
+}
