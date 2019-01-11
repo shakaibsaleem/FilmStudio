26eea733ed6c5d2f20757d33645283a84e233e22
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-11 23:05:43 +0500

    Update quantity.sql

--- ERD/quantity.sql
+++ ERD/quantity.sql
@@ -12,11 +12,11 @@
 --and (IssueDate < '2019-01-10' and (IssueDate > '2019-01-09' or (ReturnDate is NULL or ReturnDate > '2019-01-09')))
 
 
-select * from Equipments
+--select * from Equipments
 
-select * from BookedItems
+--select * from BookedItems
 
-select * from Bookings
+--select * from Bookings
 
 --where ReturnDate is not NULL
 
@@ -26,11 +26,27 @@ select * from Bookings
 --update Equipments set QuantityBooked = 0
 
 --select * from Bookings, BookedItems where (IssueDate < '2019-01-10' and (IssueDate > '2019-01-09' or (ReturnDate is NULL or ReturnDate > '2019-01-09')))
+--select combined = CAST(IssueDate as DATETIME) + CAST(IssueTime as DATETIME) from Bookings
 
-select Bookings.BookingID,EquipmentID,Quantity,IssueDate,IssueTime,DueDate,Duetime
+--select IssuedOn = IssueDate + IssueTime, DueOn = DueDate + DueTime 
+
+--select IssuedOn = cast(IssueDate as datetime) + cast(IssueTime as datetime),
+--DueOn = cast(DueDate as datetime) + cast(DueTime as datetime)
+--from
+--(select Bookings.BookingID,EquipmentID,Quantity,IssueDate,IssueTime,DueDate,Duetime
+--from BookedItems, Bookings 
+--where Bookings.BookingID = BookedItems.BookingID 
+--and IssueDate <= '2019-01-16'
+--and DueDate >= '2019-01-10'
+--and EquipmentID = 3
+--) as Fahad
+
+select Bookings.BookingID,EquipmentID,Quantity,
+IssuedOn = cast(IssueDate as datetime) + cast(IssueTime as datetime),
+DueOn = cast(DueDate as datetime) + cast(DueTime as datetime)
 from BookedItems, Bookings 
 where Bookings.BookingID = BookedItems.BookingID 
 and IssueDate <= '2019-01-16'
 and DueDate >= '2019-01-10'
 and EquipmentID = 3
-order by IssueDate,IssueTime,DueDate,DueTime
+order by IssuedOn,DueOn
