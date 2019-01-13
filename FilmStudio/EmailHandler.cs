3a40eefb6734b05f1f510593f3cf83fad5720a57
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-14 01:01:22 +0500

    Create EmailHandler.cs

---
+++ FilmStudio/EmailHandler.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EASendMail; //add EASendMail namespace, requires install
+// for help, see https://www.emailarchitect.net/easendmail/ex/c/6.aspx
+
+namespace FilmStudio
+{
+    class EmailHandler
+    {
+        SmtpMail mail;
+        SmtpClient client;
+        SmtpServer server;
+
+        public EmailHandler()
+        {
+            mail = new SmtpMail("TryIt");
+            client = new SmtpClient();
+            server = new SmtpServer("smtp.office365.com");
+
+            server.User = "ms01036@st.habib.edu.pk";
+            server.Password = "haya123-/:";
+            server.Port = 587;
+            server.ConnectType = SmtpConnectType.ConnectSSLAuto;
+
+            mail.From = "ms01036@st.habib.edu.pk";
+            mail.To = "kr03917@st.habib.edu.pk";
+            mail.Subject = "Ignore this email";
+            mail.TextBody = "Testing 1 2 3";
+        }
+
+        public EmailHandler(string user, string pass, string sender, string recipient, string subject, string body)
+        {
+            mail = new SmtpMail("Email");
+            client = new SmtpClient();
+            server = new SmtpServer("smtp.office365.com");
+
+            server.User = user;
+            server.Password = pass;
+            server.Port = 587;
+            server.ConnectType = SmtpConnectType.ConnectSSLAuto;
+
+            mail.From = sender;
+            mail.To = recipient;
+            mail.Subject = subject;
+            mail.TextBody = body;
+        }
+
+        public bool Send()
+        {
+            try
+            {
+                client.SendMail(server, mail);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Send(string recipient, string subject, string body)
+        {
+            mail.To = recipient;
+            mail.Subject = subject;
+            mail.TextBody = body;
+
+            try
+            {
+                client.SendMail(server, mail);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void SendEmail()
+        {
+            SmtpMail oMail = new SmtpMail("TryIt");
+            SmtpClient oSmtp = new SmtpClient();
+
+            // Your email address
+            oMail.From = "kr03917@st.habib.edu.pk";
+
+            // Set recipient email address
+            oMail.To = "ms01036@st.habib.edu.pk";
+
+            // Set email subject
+            oMail.Subject = "test email";
+
+            // Set email body
+            oMail.TextBody = "I HOPE YOU GET";
+
+            // If your account is office 365, please change to Office 365 SMTP server
+            SmtpServer oServer = new SmtpServer("smtp.office365.com");
+
+            // User authentication should use your
+            // email address as the user name.
+            oServer.User = "kr03917@st.habib.edu.pk";
+            oServer.Password = "Kevin1975!";
+
+            // use 587 TLS port
+            oServer.Port = 587;
+
+            // detect SSL/TLS connection automatically
+            oServer.ConnectType = SmtpConnectType.ConnectSSLAuto;
+
+            oSmtp.SendMail(oServer, oMail);
+        }
+    }
+}
