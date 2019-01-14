42e6c952c9bcb9391c214029b054da629ef1bc96
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-14 23:37:47 +0500

    Implemented Add Course

---
+++ FilmStudio/frmCourse.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FilmStudio
+{
+    partial class frmCourse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCourse));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(89, 38);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 20);
+            this.txtCode.TabIndex = 2;
+            this.txtCode.TextChanged += new System.EventHandler(this.txtCode_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Course Code";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(196, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // frmCourse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 321);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCourse";
+            this.Text = "Add Course";
+            this.Load += new System.EventHandler(this.frmCourse_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
---
+++ FilmStudio/frmCourse.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FilmStudio
+{
+    public partial class frmCourse : Form
+    {
+        public User CurrentUser;
+        Course myCourse;
+
+        mySQLcon myCon;
+        SqlConnection con;
+        string state;
+
+        public frmCourse(User currentUser)
+        {
+            InitializeComponent();
+            myCon = new mySQLcon();
+            con = myCon.con;
+            myCourse = new Course();
+            CurrentUser = currentUser;
+            state = "Empty";
+        }
+
+        public frmCourse(string id, User currentUser)
+        {
+            InitializeComponent();
+            myCon = new mySQLcon();
+            con = myCon.con;
+            myCourse = new Course();
+            CurrentUser = currentUser;
+            LoadRecord(id);
+            state = "View";
+        }
+
+        private void frmCourse_Load(object sender, EventArgs e)
+        {
+            UpdateFields(state);
+        }
+
+        private void LoadRecord(string id)
+        {
+
+        }
+
+        private void UpdateFields(string state)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter Course Name, then Add", "Name Field is blank");
+            }
+            else if (txtCode.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter Course Code, then Add", "Code Field is blank");
+            }
+            else
+            {
+                SqlDataReader rd;
+                SqlTransaction tran = con.BeginTransaction();
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = con;
+                    cmd.Transaction = tran;
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.CommandText = "insert into Courses (CourseName,CourseCode) values ('" +
+                        myCourse.CourseName + "','" + myCourse.CourseCode + "')";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "select top 1 CourseID from Courses order by CourseID desc";
+                    rd = cmd.ExecuteReader();
+                    if (rd.Read() == true)
+                    {
+                        myCourse.ID = rd[0].ToString();
+                    }
+                    rd.Close();
+                    tran.Commit();
+                    state = "Incomplete";
+                    UpdateFields(state);
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                    MessageBox.Show(ex.Message, "Error in btnAdd");
+                }
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            myCourse.CourseName = txtName.Text;
+        }
+
+        private void txtCode_TextChanged(object sender, EventArgs e)
+        {
+            myCourse.CourseCode = txtCode.Text;
+        }
+    }
+}
