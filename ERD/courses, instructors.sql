c90aa48273a67fbbafefd3b13f6f15c0a264f9e7
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-15 00:38:06 +0500

    Create courses, instructors.sql

---
+++ ERD/courses, instructors.sql
@@ -0,0 +1,7 @@
+--insert into Courses values('Film Making','FILM102')
+--select * from Courses
+--delete from Courses where CourseID > 6
+
+--insert into Instructors (Name,Contact,Email,HabibID) values ('n','0','e','i')
+--select * from Instructors
+--delete from Instructors where InstructorID > 3
