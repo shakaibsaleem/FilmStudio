ff24ef937eaa780cc7539b400b2a3bfe34fe0983
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-15 23:19:16 +0500

    implemented add student

---
+++ FilmStudio/frmStudent.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FilmStudio
+{
+    partial class frmStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmStudent));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtHabibID = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(67, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(237, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contact";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Habib ID";
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(67, 38);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(237, 20);
+            this.txtContact.TabIndex = 5;
+            this.txtContact.TextChanged += new System.EventHandler(this.txtContact_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(67, 64);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(237, 20);
+            this.txtEmail.TabIndex = 6;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txtHabibID
+            // 
+            this.txtHabibID.Location = new System.Drawing.Point(67, 90);
+            this.txtHabibID.Name = "txtHabibID";
+            this.txtHabibID.Size = new System.Drawing.Size(237, 20);
+            this.txtHabibID.TabIndex = 7;
+            this.txtHabibID.TextChanged += new System.EventHandler(this.txtHabibID_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(229, 129);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(292, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "*Note: Habib ID is usually the first part of email, before the @";
+            // 
+            // frmStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 167);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtHabibID);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmStudent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Student";
+            this.Load += new System.EventHandler(this.frmStudent_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtHabibID;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ FilmStudio/frmStudent.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FilmStudio
+{
+    public partial class frmStudent : Form
+    {
+        public User CurrentUser;
+        Student myStudent;
+
+        mySQLcon myCon;
+        SqlConnection con;
+        string state;
+
+        public frmStudent(User currentUser)
+        {
+            InitializeComponent();
+            myCon = new mySQLcon();
+            con = myCon.con;
+            myStudent = new Student();
+            CurrentUser = currentUser;
+            state = "Empty";
+        }
+
+        public frmStudent(string id, User currentUser)
+        {
+            InitializeComponent();
+            myCon = new mySQLcon();
+            con = myCon.con;
+            myStudent = new Student();
+            CurrentUser = currentUser;
+            //LoadRecord(id);
+            state = "View";
+        }
+
+        private void frmStudent_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter Student's Name, then Add", "Name Field is blank");
+            }
+            else if (txtContact.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter Student's Contact number, then Add", "Contact Field is blank");
+            }
+            else if (txtEmail.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter Student's Email address, then Add", "Email Field is blank");
+            }
+            else if (txtHabibID.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter Student's Habib ID, then Add.", "Habib ID Field is blank");
+            }
+            else
+            {
+                SqlDataReader rd;
+                SqlTransaction tran = con.BeginTransaction();
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = con;
+                    cmd.Transaction = tran;
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.CommandText = "insert into Students (Name,Contact," +
+                        "Email,HabibID) values ('" + myStudent.Name + "','" +
+                        myStudent.Contact + "','" +
+                        myStudent.Email + "','" +
+                        myStudent.HabibID + "')";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "select top 1 StudentID from Students" +
+                        " order by StudentID desc";
+                    rd = cmd.ExecuteReader();
+                    if (rd.Read() == true)
+                    {
+                        myStudent.ID = rd[0].ToString();
+                    }
+                    rd.Close();
+                    tran.Commit();
+                    state = "Incomplete";
+                    //UpdateFields(state);
+
+                    //temp scene
+                    MessageBox.Show(myStudent.Name +
+                        " has been added", "Student added");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                    MessageBox.Show(ex.Message, "Error in btnAdd");
+                }
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            myStudent.Name = txtName.Text;
+        }
+
+        private void txtContact_TextChanged(object sender, EventArgs e)
+        {
+            myStudent.Contact = txtContact.Text;
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            myStudent.Email = txtEmail.Text;
+        }
+
+        private void txtHabibID_TextChanged(object sender, EventArgs e)
+        {
+            myStudent.HabibID = txtHabibID.Text;
+        }
+    }
+}
