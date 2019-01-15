9eee69b1db3b5f2eff43a4b0eb16c2fe6752eb92
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-16 01:26:39 +0500

    Update courses, instructors.sql

--- ERD/courses, instructors.sql
+++ ERD/courses, instructors.sql
@@ -1,11 +1,15 @@
---insert into Courses values('Film Making','FILM102')
---select * from Courses
+--insert into Courses values('newcrs','crs000')
+select * from Courses
 --delete from Courses where CourseID > 6
 
---insert into Instructors (Name,Contact,Email,HabibID) values ('n','0','e','i')
---select * from Instructors
+--insert into Instructors (Name,Contact,Email,HabibID) values ('newinstructor','0','e','i')
+select * from Instructors
 --delete from Instructors where InstructorID > 3
 
---insert into Students (Name,Contact,Email,HabibID) values ('n','0','e','i')
---select * from Students
---delete from Students where StudentID > 3
+--insert into Students (Name,Contact,Email,HabibID) values ('newstudent','0','e','i')
+select * from Students
+--delete from Students where StudentID > 6
+
+--insert into Enrolments (CourseID,StudentID,InstructorID,Term) values (12,7,6,'Spring2019')
+select * from Enrolments
+--delete from Enrolments where EnrolmentID > 7
