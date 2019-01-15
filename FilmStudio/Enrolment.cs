01b1098ba5b4634cb0e1951856ca2ffd91c60b1e
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-15 23:19:42 +0500

    Update Enrolment.cs

--- FilmStudio/Enrolment.cs
+++ FilmStudio/Enrolment.cs
@@ -8,26 +8,26 @@ namespace FilmStudio
 {
     public class Enrolment
     {
-        public Student MyStudent;
-        public Course MyCourse;
-        public Instructor MyInstructor;
+        public Student Student;
+        public Course Course;
+        public Instructor Instructor;
         public string ID, Term;
 
         public Enrolment()
         {
             ID = "1";
-            MyStudent = new Student();
-            MyCourse = new Course();
-            MyInstructor = new Instructor();
+            Student = new Student();
+            Course = new Course();
+            Instructor = new Instructor();
             Term = "Spring2019";
         }
 
         public Enrolment(string iD, Student myStudent, Course myCourse, Instructor myInstructor, string term)
         {
             ID = iD ?? throw new ArgumentNullException(nameof(iD));
-            MyStudent = myStudent ?? throw new ArgumentNullException(nameof(myStudent));
-            MyCourse = myCourse ?? throw new ArgumentNullException(nameof(myCourse));
-            MyInstructor = myInstructor ?? throw new ArgumentNullException(nameof(myInstructor));
+            Student = myStudent ?? throw new ArgumentNullException(nameof(myStudent));
+            Course = myCourse ?? throw new ArgumentNullException(nameof(myCourse));
+            Instructor = myInstructor ?? throw new ArgumentNullException(nameof(myInstructor));
             Term = term ?? throw new ArgumentNullException(nameof(term));
         }
     }
