24fff1b970f42f7e9509405bbedef891657f1325
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-17 00:49:04 +0500

    Update frmEquipment.cs

--- FilmStudio/frmEquipment.cs
+++ FilmStudio/frmEquipment.cs
@@ -24,30 +24,95 @@ namespace FilmStudio
             myEquipment = new Equipment();
         }
 
-        public frmEquipment(Equipment eq)
-        {
-            InitializeComponent();
-            //myEquipment = new Equipment(eq.Id, eq.QtyAvailable, eq.QtyBooked, eq.Description, eq.Make, eq.Model, eq.Remarks);
-            myEquipment = new Equipment(eq.ID, eq.QtyAvailable, eq.QtyBooked, eq.Description, eq.Remarks);
-        }
+        //public frmEquipment(Equipment eq)
+        //{
+        //    InitializeComponent();
+        //    //myEquipment = new Equipment(eq.Id, eq.QtyAvailable, eq.QtyBooked, eq.Description, eq.Make, eq.Model, eq.Remarks);
+        //    myEquipment = new Equipment(eq.ID, eq.QtyAvailable, eq.QtyBooked, eq.Description, eq.Remarks);
+        //}
 
         private void frmEquipment_Load(object sender, EventArgs e)
         {
             myCon = new mySQLcon();
             con = myCon.con;
 
-            txtItemId.Text = myEquipment.ID.ToString();
-            txtQtyAvailable.Text = myEquipment.QtyAvailable.ToString();
-            txtQtyBooked.Text = myEquipment.QtyBooked.ToString();
-            //txtMake.Text = myEquipment.Make.ToString();
-            //txtModel.Text = myEquipment.Model.ToString();
-            txtDescription.Text = myEquipment.Description.ToString();
-            txtRemarks.Text = myEquipment.Remarks.ToString();
+            //txtItemId.Text = myEquipment.ID.ToString();
+            //txtQtyAvailable.Text = myEquipment.QtyAvailable.ToString();
+            //txtQtyBooked.Text = myEquipment.QtyBooked.ToString();
+            ////txtMake.Text = myEquipment.Make.ToString();
+            ////txtModel.Text = myEquipment.Model.ToString();
+            //txtDescription.Text = myEquipment.Description.ToString();
+            //txtRemarks.Text = myEquipment.Remarks.ToString();
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtDescription.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter a Description to identify this item, then Add", "Description field is blank");
+            }
+            else if (txtQuantity.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter total quantity of this item, then Add", "Quantity field is blank");
+            }
+            else
+            {
+                SqlDataReader rd;
+                SqlTransaction tran = con.BeginTransaction();
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = con;
+                    cmd.Transaction = tran;
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.CommandText = "insert into Equipments (Description, " +
+                        "QuantityTotal,Remarks) values ('" + myEquipment.Description +
+                        "','" + Convert.ToInt32(myEquipment.QuantityTotal) + "','" + myEquipment.Remarks + "')";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "select top 1 EquipmentID from Equipments order by EquipmentID desc";
+                    rd = cmd.ExecuteReader();
+                    if (rd.Read() == true)
+                    {
+                        myEquipment.ID = rd[0].ToString();
+                    }
+                    rd.Close();
+                    tran.Commit();
+                    //state = "Incomplete";
+                    //UpdateFields(state);
+
+                    //temp scene
+                    MessageBox.Show(myEquipment.Description + " has been added to Equipments", "Equipment added");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                    MessageBox.Show(ex.Message, "Error in btnAdd");
+                }
+            }
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+            myEquipment.QuantityTotal = Convert.ToInt32(txtQuantity.Text);
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+            myEquipment.Description = txtDescription.Text;
+        }
+
+        private void txtRemarks_TextChanged(object sender, EventArgs e)
+        {
+            myEquipment.Remarks = txtRemarks.Text;
         }
     }
 }
