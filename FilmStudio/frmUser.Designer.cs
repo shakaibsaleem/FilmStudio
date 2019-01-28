2f377518192f8c3f5ba42615813c7c5e32741c53
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-01-28 21:30:40 +0500

    Update frmUser.Designer.cs

--- FilmStudio/frmUser.Designer.cs
+++ FilmStudio/frmUser.Designer.cs
@@ -146,6 +146,7 @@
             this.btnEdit.TabIndex = 11;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Visible = false;
             this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // btnNext
